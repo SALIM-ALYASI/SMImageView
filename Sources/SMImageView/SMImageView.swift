7b0b34f399c2b6f329ce11903ad6dee43b64f02c
الياسي والأمل <r.m.alyasi@gmail.com>
2021-06-28 22:48:24 +0400

    alert

--- Sources/SMImageView/SMImageView.swift
+++ Sources/SMImageView/SMImageView.swift
@@ -2,8 +2,8 @@
 //    var text = "Hello, World!"
 //}
 import UIKit
- class ShowSMImageView {
-    static let instance = ShowSMImageView()
+public struct ShowSMImageView {
+    public init() {}
      func alert(arry:[String],indexPath:Int)->Showhispicture{
         let storyBoard = UIStoryboard(name: "AlertStoryboard", bundle: .main)
         let alertVC = storyBoard.instantiateViewController(withIdentifier: "AlertVC") as! Showhispicture
