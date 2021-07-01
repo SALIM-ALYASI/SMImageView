eb0a558f00b13aa2d98958cf3e483883c0052bf3
الياسي والأمل <r.m.alyasi@gmail.com>
2021-07-01 13:35:41 +0400

    World

--- Sources/SMImageView/ShowSMImageView.swift
+++ Sources/SMImageView/ShowSMImageView.swift
@@ -3,9 +3,11 @@ struct SMImageView {
 }
 import UIKit
 public class Hello: UIViewController {
+    var World = 0
+    
    public override func viewDidLoad() {
         super.viewDidLoad()
-       
+       print(World)
         view.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
         
     }
